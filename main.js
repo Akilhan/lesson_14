8975addc68c2fd76d46fe6053bdf0d25bdaa8049
Akilhan <oqiljondadaxanov@gmail.com>
2023-09-11 03:47:27 +0500

    all finished

---
+++ main.js
@@ -0,0 +1,15 @@
+const plus = document.getElementById("plus");
+const more = document.getElementById("more");
+const minus = document.getElementById("minus");
+
+plus.addEventListener("click", e =>{
+    e.preventDefault();
+
+    if( more.style.display = "none"){
+        return more.style.display = "block"
+
+    }else if(more.style.display = "none"){
+        minus.style.display = inline;
+    }
+
+});
